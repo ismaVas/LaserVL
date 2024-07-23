c712f83d34190473ed7b98ef312e17b4c876262f
VasIsmael <ismael.vas@vl-machines.fr>
2024-07-23 12:20:42 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿
+using LaserVL.config;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+
+namespace LaserVL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private LaserConfigs laserConfigs;
+        private TcpClient connexion;
+        private NetworkStream stream;
+        public MainWindow()
+        {
+            InitializeComponent();
+            laserConfigs = LaserConfigs.LoadLaserConfigs(Path.Combine("config", "LaserConfigs.xml"));
+            
+        }
+
+
+        public bool TryConnection()
+        {
+            try
+            {
+                this.connexion = new TcpClient(laserConfigs.IpAddress, laserConfigs.Port);
+                this.stream = connexion.GetStream();
+                return true;
+            }
+            catch { }
+            return false;
+        }
+
+        ~MainWindow()
+        {
+            if (!this.isConnexionEstablished()) return;
+            this.stream.Dispose();
+            this.connexion.Dispose();
+        }
+        public bool isConnexionEstablished()
+        {
+            return this.connexion != null;
+        }
+
+        private void Button_Stop(object sender, RoutedEventArgs e)
+        {
+            if (!this.isConnexionEstablished()) return;
+            this.stream.Dispose();
+            this.connexion.Dispose();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(laserConfigs?.commands?.CommandList.FirstOrDefault(cmd => cmd.Name == "ReadDeviceStatus").Send);
+            this.stream.Write(data, 0, data.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = this.stream.Read(buffer, 0, buffer.Length);
+            LogTextBox.Text = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        }
+    }
+}
---
+++ config/LaserConfigs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace LaserVL.config
+{
+    [Serializable]
+    public class LaserConfigs
+    {
+        public string IpAddress {  get; set; }
+        public int Port {  get; set; }
+        public int BaudRate {  get; set; }
+        public int DataBit {  get; set; }
+        public int StopBit { get; set; }
+        public bool PartyBit { get; set; }
+        public bool ControlFlow { get; set; }
+        [XmlElement("Commands")]
+        public Commands commands { get; set; }
+
+        public static LaserConfigs LoadLaserConfigs(string configFile)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(LaserConfigs));
+            using(FileStream fs = new FileStream(configFile, FileMode.Open, FileAccess.Read)) {
+                return (LaserConfigs)serializer.Deserialize(fs);
+            }
+        }
+    }
+
+    [Serializable]
+    public class Commands
+    {
+        [XmlElement("Command")]
+        public Command[] CommandList { get; set; }
+    }
+
+    [Serializable]
+    public class Command
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute] 
+        public string Send { get; set; }
+        [XmlAttribute]
+        public string Return { get; set; }
+    }
+
+}
